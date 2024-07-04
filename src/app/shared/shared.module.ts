d90b9a384600653d401ff9439437882f68dbfe7a
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-03 20:48:20 -0600

    feat: creating all the components from the app

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    Error404PageComponent
+  ],
+  imports: [],
+  exports: [
+    Error404PageComponent
+  ]
+})
+export class SharedModule { }
