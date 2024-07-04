5dca11cf795b324135db31acaf0a83af44d511e5
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-04 00:50:17 -0600

    feat: Path de las rutas faltantes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { NewPageComponent } from './pages/new-page/new-page.component';
+import { LayoutPageComponent }  from './pages/layout-page/layout-page.component';
+import { NewPageComponent }     from './pages/new-page/new-page.component';
+import { SearchPageComponent }  from './pages/search-page/search-page.component';
+import { HeroPageComponent }    from './pages/hero-page/hero-page.component';
+import { ListPageComponent }    from './pages/list-page/list-page.component';
 
 const routes: Routes = [
   {
@@ -9,6 +12,11 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'new-hero', component: NewPageComponent },
+      { path: 'search',   component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list',     component: ListPageComponent },
+      { path: ':id',      component: HeroPageComponent },
+      { path: '**',       redirectTo: 'list' },
     ]
   }
 ];
