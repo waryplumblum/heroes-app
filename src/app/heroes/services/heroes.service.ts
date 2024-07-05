80486870128c80b69f05f87ac7a789e3e594edc3
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-05 17:07:14 -0600

    Revert "feat: deploy with static information"
    This reverts commit 5aa5718440634a163da9f3a1975b6af93300e5d2.

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
@@ -11,55 +11,19 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroes(): Observable<Hero[]> {
-    if (environments.useStaticData) {
-      console.log('Fetching heroes from static data');
-      return this.http.get<{ heroes: Hero[] }>('assets/data/db.json')
-        .pipe(
-          map(response => response.heroes || []),
-          catchError((error) => {
-            console.error('Error fetching static data:', error);
-            return of([]);
-          })
-        );
-    } else {
-      console.log('Fetching heroes from API');
-      return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
-        .pipe(
-          catchError((error) => {
-            console.error('Error fetching dynamic data:', error);
-            return of([]);
-          })
-        );
-    }
+  getHeroes():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
   getHeroById( id:string ): Observable<Hero|undefined> {
-    if ( environments.useStaticData ) {
-      return this.getHeroes()
-        .pipe(
-          map( heroes => heroes.find( hero => hero.id === id)),
-          catchError( () => of( undefined ))
-        );
-    } else {
-      return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
       .pipe(
         catchError( () => of(undefined) )
       )
-    }
   }
 
   getSuggestions( query:string ):Observable<Hero[]> {
-    if ( environments.useStaticData ) {
-      return this.getHeroes()
-        .pipe(
-          map(heroes => heroes
-            .filter(hero => hero.superhero.toLowerCase().includes(query.toLowerCase())).slice(0, 6)),
-          catchError(() => of([]))
-      );
-    } else {
-      return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
-    }
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
   }
 
 }
