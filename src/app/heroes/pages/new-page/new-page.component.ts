a770d1b9209d56b70211cebbd1cca01527d1b878
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-08 11:35:02 -0600

    feat: Snackbars and redirections

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,6 +4,7 @@ import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
@@ -30,7 +31,8 @@ export class NewPageComponent implements OnInit{
   constructor(
     private heroesService :HeroesService,
     private activatedRoute:ActivatedRoute,
-    private router        :Router
+    private router        :Router,
+    private snackbar      :MatSnackBar
    ) {}
 
   get currentHero():Hero{
@@ -58,12 +60,21 @@ export class NewPageComponent implements OnInit{
       this.heroesService.updateHero( this.currentHero )
         .subscribe( hero => {
           // TODO: mostar snackbar
+          this.showSnackbar(`${ hero.superhero } updated!`);
         });
       return;
     }
     this.heroesService.addHero( this.currentHero )
       .subscribe( hero => {
         // TODO: mostrar snackbar y navegar a /heroes/edit/hero.id
+        this.router.navigate(['heroes/edit',hero.id]);
+        this.showSnackbar(`${ hero.superhero } created!`);
       });
+  }
+
+  showSnackbar( message:string ):void {
+    this.snackbar.open( message, 'done',{
+      duration: 2500,
+    })
   }
 }
